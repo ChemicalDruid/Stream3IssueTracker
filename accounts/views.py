50c12d1e71b659ea2b4966387f78be439b6a527c
Athanasios Platsas <sakis.platsas@gmail.com>
2018-06-16 17:30:37 +0100

    previous login info added

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,9 @@ from django.core.urlresolvers import reverse
 from django.template.context_processors import csrf
 from accounts.forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.contrib.auth.signals import user_logged_out
+
 
 # Create your views here.
 def register(request):
@@ -14,24 +17,25 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
- 
+
             user = auth.authenticate(email=request.POST.get('email'),
                                      password=request.POST.get('password1'))
- 
+
             if user:
                 messages.success(request, "You have successfully registered")
                 return redirect(reverse('profile'))
- 
+
             else:
                 messages.error(request, "unable to log you in at this time!")
- 
+
     else:
         form = UserRegistrationForm()
- 
+
     args = {'form': form}
     args.update(csrf(request))
- 
+
     return render(request, 'register.html', args)
+
 
 @login_required(login_url='/login/')
 def profile(request):
@@ -44,20 +48,28 @@ def login(request):
         if form.is_valid():
             user = auth.authenticate(email=request.POST.get('email'),
                                      password=request.POST.get('password'))
- 
+
             if user is not None:
                 auth.login(request, user)
                 messages.error(request, "You have successfully logged in")
                 return redirect(reverse('profile'))
             else:
                 form.add_error(None, "Your email or password was not recognised")
- 
+
     else:
         form = UserLoginForm()
- 
-    args = {'form':form}
+
+    args = {'form': form}
     args.update(csrf(request))
     return render(request, 'login.html', args)
+
+
+def update_previous_login(sender, user, **kwargs):
+    user.previous_login = timezone.now()
+    user.save(update_fields=['previous_login'])
+
+
+user_logged_out.connect(update_previous_login)
 
 
 def logout(request):
